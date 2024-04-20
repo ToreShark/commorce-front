29a20f5fc620e82544b1e71759c2cfb6c23d82b2
torekhanmukhtarov <nahkerot@gmail.com>
2024-04-20 16:39:45 +0500

    added shop page and product

---
+++ app/shop/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+import { fetchProducts } from "../lib/data";
+import ShopPageComponent from "../ui/categoryDetail";
+
+export default async function ShopPage() {
+    const products = await fetchProducts();
+
+    return (
+        <>
+            <ShopPageComponent products={products} />
+        </>
+    )
+}
