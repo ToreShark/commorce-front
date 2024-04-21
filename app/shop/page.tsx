c58390b5156b54306d933e007716a97da428f881
torekhanmukhtarov <nahkerot@gmail.com>
2024-04-21 23:05:19 +0500

    added filter category

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import { fetchProducts } from "../lib/data";
 import { Product } from "../lib/interfaces/product.interface";
 import ShopPageComponent from "../ui/categoryDetail";
@@ -9,8 +9,28 @@ export default function ShopPage() {
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(100);
   const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<
+    { id: string; name: string | undefined }[]
+  >([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
   //   const products = (await fetchProducts()) || [];
 
+  const extractCategories = (products: Product[]) => {
+    const uniqueCategories = [];
+    const map = new Map();
+
+    for (const product of products) {
+      if (!map.has(product.categoryId)) {
+        map.set(product.categoryId, true); // установка уникальных id категорий
+        uniqueCategories.push({
+          id: product.categoryId,
+          name: product.categoryName,
+        });
+      }
+    }
+
+    return uniqueCategories;
+  };
   const calculatePriceRange = (products: any[]) => {
     let min = Number.POSITIVE_INFINITY;
     let max = Number.NEGATIVE_INFINITY;
@@ -33,6 +53,7 @@ export default function ShopPage() {
         setMinPrice(min);
         setMaxPrice(max);
         setProducts(initialProducts); // Устанавливаем продукты только если они не null
+        setCategories(extractCategories(initialProducts));
       } else {
         setProducts([]); // Если initialProducts null, устанавливаем пустой массив
       }
@@ -45,26 +66,42 @@ export default function ShopPage() {
     setProducts(updatedProducts || []);
   };
 
+  const handleFilterUpdate = async (categoryId?: string) => {
+    console.log(categoryId);
+    const updatedProducts = await fetchProducts(
+      minPrice,
+      maxPrice,
+      categoryId !== undefined ? categoryId : undefined
+    );
+    setProducts(updatedProducts || []);
+    setSelectedCategoryId(categoryId || "");
+  };
+
   return (
     <div className="px-4 sm:px-8">
       <div className="hidden md:grid grid-cols-4 gap-4">
-        {/* <div className="col-span-1">
-          <input
-            type="number"
-            value={minPrice}
-            onChange={(e) => setMinPrice(Number(e.target.value))}
-            placeholder="Min Price"
-          />
-          <input
-            type="number"
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(Number(e.target.value))}
-            placeholder="Max Price"
-          />
-          <button onClick={handlePriceUpdate}>Apply Prices</button>
-        </div> */}
         <div className="col-span-1">
-          <div className="mb-4 flex items-center">
+          {/* Блок с категориями */}
+          <div className="mb-4">
+            <div className="grid gap-2">
+              {categories.map(({ id, name }) => (
+                <button
+                  key={id}
+                  onClick={() => handleFilterUpdate(id)}
+                  className={`py-2 px-4 rounded-md border ${
+                    selectedCategoryId === id
+                      ? "bg-indigo-500 text-white"
+                      : "bg-white text-gray-800"
+                  } shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+                >
+                  {name}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          {/* Блок с ценами */}
+          <div className="flex items-center">
             <div className="mr-4">
               <label
                 htmlFor="minPrice"
@@ -75,7 +112,7 @@ export default function ShopPage() {
               <input
                 type="number"
                 id="minPrice"
-                value={minPrice || ""} // Установка пустой строки, если minPrice равно 0
+                value={minPrice || ""}
                 onChange={(e) => setMinPrice(parseInt(e.target.value) || 0)}
                 placeholder="Мин. цена"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
@@ -92,7 +129,7 @@ export default function ShopPage() {
               <input
                 type="number"
                 id="maxPrice"
-                value={maxPrice || ""} // Установка пустой строки, если maxPrice равно 0
+                value={maxPrice || ""}
                 onChange={(e) => setMaxPrice(parseInt(e.target.value) || 0)}
                 placeholder="Макс. цена"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
