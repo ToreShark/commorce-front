6a03241857650250bd271433e8cb497ced836b2c
torekhanmukhtarov <nahkerot@gmail.com>
2024-04-22 00:16:24 +0500

    addapted mobile

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,9 +4,11 @@ import { useCategories } from "@/app/lib/CategoryContext";
 import Link from "@/node_modules/next/link";
 import { usePathname } from "@/node_modules/next/navigation";
 import { ShoppingBag, User, Settings, FileText } from "lucide-react";
+import { useState } from "react";
 
 export default function NavBar() {
   const pathname = usePathname();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   const links = [
     { name: "Главная", href: "/" },
@@ -26,7 +28,7 @@ export default function NavBar() {
         </Link>
         <nav className="hidden gap-12 lg:flex 2xl:ml-16">
           {links.map((link, idx) => (
-            <div key={idx}>
+            <div key={idx}  className="flex items-center">
               {pathname === link.href ? (
                 <Link
                   className="text-lg font-semibold text-primary"
@@ -46,7 +48,104 @@ export default function NavBar() {
           ))}
         </nav>
 
-        <div className="flex divide-x border-r sm:border-l">
+        <div className="flex items-center lg:hidden">
+          <button
+            className="navbar-burger flex items-center px-3 py-2 border rounded text-primary border-primary"
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+
+        {/* Slide-out navigation panel for mobile */}
+        <div
+          className={`${
+            isNavOpen
+              ? "translate-x-0 opacity-100"
+              : "translate-x-full opacity-0"
+          } absolute flex flex-col items-center justify-between bg-white dark:bg-slate-800 dark:text-white transition duration-500 ease-in-out transform top-0 left-0 right-0 bottom-0 lg:hidden z-50 pt-2`}
+        >
+          <button className="self-end m-4" onClick={() => setIsNavOpen(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-gray-600 dark:text-gray-300"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <div className="flex flex-col items-center">
+          <nav className="mb-b">
+            {links.map((link, idx) => (
+              <div key={idx} className="mb-8">
+                <Link
+                  href={link.href}
+                  className="text-lg font-semibold text-gray-600 dark:text-gray-300 hover:text-primary dark:hover:text-primary"
+                  onClick={() => setIsNavOpen(false)}
+                >
+                  {link.name}
+                </Link>
+              </div>
+            ))}
+          </nav>
+          <div className="flex flex-col items-center mb-8">
+            <Button
+              variant={"outline"}
+              className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none mb-4"
+            >
+              <ShoppingBag />
+              <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                Корзина
+              </span>
+            </Button>
+            <Button
+              variant="outline"
+              className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none mb-4"
+            >
+              <User />
+              <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                Аккаунт
+              </span>
+            </Button>
+            <Button
+              variant="outline"
+              className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none mb-4"
+            >
+              <Settings />
+              <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                Админ
+              </span>
+            </Button>
+            <Button
+              variant="outline"
+              className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+            >
+              <FileText />
+              <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                История
+              </span>
+            </Button>
+          </div>
+          </div>
+          
+
+        </div>
+
+        <div className="flex divide-x border-r sm:border-l hidden lg:flex">
           <Button
             variant={"outline"}
             // onClick={() => handleCartClick()}
