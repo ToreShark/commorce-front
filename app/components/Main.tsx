4f3c6bb393588bc8680dbe45f48d3896f13f2283
torekhanmukhtarov <nahkerot@gmail.com>
2024-06-02 16:43:12 +0500

    #2 done

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -18,11 +18,11 @@ export default async function Main() {
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Top Fashion for a top price!
+            Лучший товар, лучшая цена!
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            We sell only the most exclusive and high quality products for you.
-            We are the best so come and shop with us.
+            Только отборные и качественные товары для вас.
+            Мы знаем, что вам нужно. Приходите и выбирайте.
           </p>
         </div>
 
