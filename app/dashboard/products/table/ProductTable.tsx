33d60d2973337ff6232cb66b28714d7564186e62
torekhanmukhtarov <nahkerot@gmail.com>
2024-07-27 10:43:10 +0500

    #4 change refresh Product Table image

--- app/dashboard/products/table/ProductTable.tsx
+++ app/dashboard/products/table/ProductTable.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -21,6 +22,50 @@ export default function ProductTable({
   onEdit,
   onDelete,
 }: ProductTableProps) {
+  const [selectedImage, setSelectedImage] = useState<{ [key: string]: string }>(
+    {}
+  );
+  
+  useEffect(() => {
+    products.forEach((product) => {
+      if (product.images && product.images.length > 0) {
+        const imagePaths = product.images.map((image) => image.imagePath);
+        selectRandomImage(product.id, imagePaths);
+      }
+    });
+  }, [products]);
+
+  const selectRandomImage = async (productId: string, imagePaths: string[]) => {
+    let selectedImage = "";
+  
+    for (const imagePath of imagePaths) {
+      const exists = await checkImageExists(`${process.env.NEXT_PUBLIC_API_URL}${imagePath}`);
+      if (exists) {
+        selectedImage = imagePath;
+        break;
+      }
+    }
+  
+    if (!selectedImage && imagePaths.length > 0) {
+      selectedImage = imagePaths[0]; // Fallback to the first image if all attempts fail
+    }
+  
+    setSelectedImage((prev) => ({
+      ...prev,
+      [productId]: selectedImage,
+    }));
+  };
+
+  const checkImageExists = (url: string): Promise<boolean> => {
+    return new Promise((resolve) => {
+      const img = document.createElement('img');
+      img.src = url;
+      img.onload = () => resolve(true);
+      img.onerror = () => resolve(false);
+    });
+  };
+  
+
   if (!products || products.length === 0) {
     return (
       <TableContainer component={Paper}>
@@ -55,10 +100,10 @@ export default function ProductTable({
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell>
-                {product.mainImagePath ? (
+                {selectedImage[product.id] ? (
                   <img
                     src={`${process.env.NEXT_PUBLIC_API_URL}${
-                      product.mainImagePath
+                      selectedImage[product.id]
                     }?${new Date().getTime()}`}
                     alt={product.name}
                     width={50}
@@ -66,6 +111,7 @@ export default function ProductTable({
                     style={{ objectFit: "cover" }}
                     onError={(e) => {
                       e.currentTarget.src = "/placeholder.png";
+                      e.currentTarget.onerror = null;
                     }}
                   />
                 ) : (
