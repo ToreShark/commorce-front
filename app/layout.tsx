b96936f69609bba4b85de87a291056adde65d27c
torekhanmukhtarov <nahkerot@gmail.com>
2024-08-07 00:42:17 +0500

    #4 change meta pixel AK

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '742823994416466');
+            fbq('init', '356355547516516');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -50,7 +50,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=742823994416466&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=356355547516516&ev=PageView&noscript=1"
             alt="facebook pixel"
           />
         </noscript>
